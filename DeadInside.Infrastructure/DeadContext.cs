505088225f2d04d70f53a4c9c49545beceec8952
rlali <r.lali@compassplus.ru>
2024-08-02 13:51:56 +0500

    nobody

---
+++ DeadInside.Infrastructure/DeadContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DeadInside.Infrastructure
+{
+    public class DeadContext : 
+        DbContext
+    {
+        public DeadContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
